02edb3a324ee1afc9cb3ffcc289a5e389de7767e
mirandalily <mirandaraymond@me.com>
2016-11-03 13:31:23 -0700

    setting up bower

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require angular
+//= require angular-rails-templates
+//= require angular-ui-router
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require bootstrap/dist/js/bootstrap
 //= require_tree .
+//= require angular-rails-templates
