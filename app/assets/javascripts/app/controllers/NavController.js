562f8d401764b472a9aa467422fc9d53d4bda2f3
mirandalily <mirandaraymond@me.com>
2016-11-10 11:52:51 -0800

    navcontroller scope devise

--- app/assets/javascripts/app/controllers/NavController.js
+++ app/assets/javascripts/app/controllers/NavController.js
@@ -1,9 +1,25 @@
 function NavController($state, $scope, Auth) {
-  
+
   $scope.signedIn = Auth.isAuthenticated;
   $scope.logout = Auth.logout;
 
+  Auth.currentUiser()
+    .then(function(user) {
+      $scope.user = user;
+    });
 
+    $scope.$on('devise:login', function (e, user){
+      $scope.user = user;
+    });
+
+    $scope.$on('devise:logout', function (e, user){
+      $scope.user = {};
+    });
+
+    $scope.$on('devise:new-registration', function (e, user){
+      $scope.user = user;
+    });
+    
 }
 
 angular
