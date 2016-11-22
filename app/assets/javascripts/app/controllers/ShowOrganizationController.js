4deeaa0a367158da4a3260b2d051560d1aa35caf
mirandalily <mirandaraymond@me.com>
2016-11-22 14:48:35 -0800

    completed Show Controller for organizations and created an Edit Organization controller

--- app/assets/javascripts/app/controllers/ShowOrganizationController.js
+++ app/assets/javascripts/app/controllers/ShowOrganizationController.js
@@ -1,6 +1,24 @@
-function ShowOrganizationController(organization, OrganizationService) {
-  this.organization = organization.data;
+function ShowOrganizationController($stateParams, $state, Auth, $controller, OrganizationService) {
 
+  var ctrl = this;
+
+  Auth.currentUser()
+    .then(function(user) {
+      ctrl.user = user;
+    });
+
+    ctrl.organization = OrganizationService.get({ id: $stateParams.id });
+
+    ctrl.deleteOrganization = function(organization) {
+      if (ctrl.user.id == organization.user_id) {
+        organization.$delete(function() {
+          alert('Deleted Organization.')
+          $state.go('home.organizations');
+        });
+      } else {
+        alert('Something went wrong.')
+      }
+    }
 
 }
 
