287552947152f77183f7059f766c8fa2392e55b1
mirandalily <mirandaraymond@me.com>
2016-12-08 12:41:21 -0800

    changing database to add categories, and add donations directly to organization model as an array

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103221800) do
+ActiveRecord::Schema.define(version: 20161208203823) do
 
-  create_table "donations", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20161103221800) do
     t.boolean  "volunteers"
     t.string   "address"
     t.integer  "user_id"
+    t.string   "donations"
+    t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
