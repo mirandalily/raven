478cef0ab1470cc4df303718bafbbeff438b6796
mirandalily <mirandaraymond@me.com>
2016-12-19 14:41:05 -0800

    added comment info to organization and user models

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,8 @@
 class Organization < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
-
+  has_many :comments
+  
   def donations=(str)
     write_attribute( :donations, str.split('/r/n') )
   end
